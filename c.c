2acc98314c4320e1817edcd5d3a821a5f7e29e2a
ankitsinghrawat-1 <ankitrawat54328@gmail.com>
2025-07-12 23:04:20 +0530

    passorfail

--- c.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() 
-{
-    char a[] = 'ankit';
-    printf("The value of a is: %s\n", a);
-
-    return 0;
-}
---
+++ passorfail.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() 
+{
+    int phy, chem, maths, mark, marks;
+    printf("Enter your physics marks: ");
+    scanf("%d", &phy);
+    printf("Enter your chemistry marks: ");
+    scanf("%d", &chem);
+    printf("Enter your maths marks: ");
+    scanf("%d", &maths);
+    mark = phy + chem + maths;
+    marks = mark/3;
+
+    if (phy < 33 || chem < 33 || maths < 33) {
+        printf("You have failed in one or more subjects.\n");
+        if (marks < 40) {
+            printf("You have failed overall as well.\n");
+        } else {
+            printf("You have passed overall but failed in one or more subjects.\n");
+        }
+    } else {
+        printf("You have passed all subjects.\n");
+    }
+    
+    return 0;
+}
