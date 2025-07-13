3547ee9b0a7950049ff8aa50f226bfe3ac540293
ankitsinghrawat-1 <ankitrawat54328@gmail.com>
2025-07-13 22:15:13 +0530

    reverse multiply

--- revmultiply.c
+++ revmultiply.c
@@ -5,7 +5,7 @@ int main()
     int n;
     printf("Enter a number: ");
     scanf("%d", &n);
-    for (int i = 10; i > 0; i--) {
+    for (int i = 10; i; i--) {
         printf("%d x %d = %d\n", n, i, n * i);
 
     }
