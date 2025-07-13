707335f18b2f39566591e1aea911acf6121a764c
ankitsinghrawat-1 <ankitrawat54328@gmail.com>
2025-07-13 09:24:39 +0530

    taxcalc

--- taxcalc.c
+++ taxcalc.c
@@ -2,27 +2,35 @@
 
 int main() 
 {
-    int salary, income, tax;
-    printf("Enter your salary:");
+    int salary;
+    double income, tax = 0.0;
+    printf("Enter your salary: ");
     scanf("%d", &salary);
-    income = salary*12;
-    if (income < 250000){
-        printf("You don't have to pay tax\n");
+
+    if (salary < 0) {
+        printf("Invalid salary. Please enter a non-negative value.\n");
+        return 1;
     }
-    else if (income >= 250000 && income < 500000){
-        printf("You have to pay 5%% tax\n");
-        tax = income * 0.05;
-        printf("Your tax is %d\n", tax);
+
+    income = salary * 12;
+
+    if (income > 1000000) {
+        tax += (income - 1000000) * 0.30;
+        income = 1000000;
     }
-    else if (income >= 500000 && income < 1000000){
-        printf("You have to pay 10%% tax\n");
-        tax = income * 0.10;
-        printf("Your tax is %d\n", tax);
-    } 
-    else {
-        printf("You have to pay 30%% tax\n");
-        tax = income * 0.30;
-        printf("Your tax is %d\n", tax);
+    if (income > 500000) {
+        tax += (income - 500000) * 0.10;
+        income = 500000;
     }
+    if (income > 250000) {
+        tax += (income - 250000) * 0.05;
+    }
+
+    if (tax == 0) {
+        printf("You don't have to pay tax.\n");
+    } else {
+        printf("Your total tax is %.2f\n", tax);
+    }
+
     return 0;
 }
