b1e04039bf2c8675bd3dc4bf5a9e8d6093390a84
ankitsinghrawat-1 <ankitrawat54328@gmail.com>
2025-07-18 17:32:09 +0530

    factorial with recurssion

---
+++ factorialwithrecurssion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int factorial (int);
+
+int factorial(int x){
+    if (x == 0) {
+        return 1;
+    } else {
+        return x * factorial(x - 1);
+    }
+
+}
+
+int main() 
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    printf("Factorial of %d is %d", n, factorial(n));
+    return 0;
+}
