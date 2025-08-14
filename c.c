5007fe05730e7cc02c27376d678f51df4e1f3748
ankitsinghrawat-1 <ankitrawat54328@gmail.com>
2025-08-14 19:17:37 +0530

    fibbonaci Nth number

---
+++ c.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int fibbonacci(int);
+int fibbonacci(int n)
+{
+    if (n == 0 || n == 1)
+    {
+        return n;
+    }
+    else
+    {
+        return fibbonacci(n - 1) + fibbonacci(n - 2);
+    }
+        if (n < 0)
+        {
+            printf("Enter the positive number");
+        }
+
+}
+int main()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    printf("Fibonacci of %d is %d\n", n, fibbonacci(n));
+    return 0;
+}
