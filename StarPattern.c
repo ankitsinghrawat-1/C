5d77e82d23ece0b0e776b239ea005feb87394122
ankitsinghrawat-1 <ankitrawat54328@gmail.com>
2025-08-15 12:39:25 +0530

    created a star pattern

---
+++ StarPattern.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() 
+{
+    int n;
+    printf("Enter a number to create a star pattern: ");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < (2*i)+1; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
